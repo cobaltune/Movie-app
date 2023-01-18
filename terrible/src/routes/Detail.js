9c6945cd6fa868be84f55b384961cca82c997cd1
cobaltune <cobaltune@naver.com>
2023-01-19 01:58:26 +0900

    chore: Delete `useCallback`

--- terrible/src/routes/Detail.js
+++ terrible/src/routes/Detail.js
@@ -3,16 +3,16 @@ import { useParams } from 'react-router-dom';
 
 function Detail() {
   const { id } = useParams();
-  const getMovie = useCallback(async () => {
+  const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
     console.log(json);
-  }, [id]);
+  };
 
   useEffect(() => {
     getMovie();
-  }, [getMovie]);
+  }, [id]);
   return <h1>Detail</h1>;
 }
 
