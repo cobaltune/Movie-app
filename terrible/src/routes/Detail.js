f5086099990d702959736c8d6c55ef05fa97b2ef
cobaltune <cobaltune@naver.com>
2023-01-22 02:42:35 +0900

    feat: Add loading

--- terrible/src/routes/Detail.js
+++ terrible/src/routes/Detail.js
@@ -1,19 +1,28 @@
-import { useEffect, useCallback } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Loading from './Loading';
+import styles from './Detail.module.css';
 
 function Detail() {
   const { id } = useParams();
+  const [loading, setLoading] = useState(true);
+
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    setLoading(false);
   };
 
   useEffect(() => {
     getMovie();
   }, [id]);
-  return <h1>Detail</h1>;
+
+  return (
+    <div className={styles.container}>
+      {loading ? Loading() : <p>Detail</p>}
+    </div>
+  );
 }
 
 export default Detail;
