03aea14ff16fa818a67633d795bd2a9f31f7c118
cobaltune <cobaltune@naver.com>
2023-01-22 02:44:58 +0900

    chore: Delete Navigation

--- terrible/src/index.js
+++ terrible/src/index.js
@@ -2,12 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles.css';
-import { createRoot } from 'react-dom/client';
-import { Navigation } from './components/Components';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
-
-const navDomNode = document.getElementById('navigation');
-const navRoot = createRoot(navDomNode);
-navRoot.render(<Navigation />);
