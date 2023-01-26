96ff99d75d63a929725511955b66164c4f094878
Dorabang <psa1195@naver.com>
2023-01-27 04:00:22 +0900

    chore: Add wrap, remove unnecessary code

--- terrible/src/routes/Detail.js
+++ terrible/src/routes/Detail.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import Loading from '../components/Loading';
-import Movie from '../components/Movie';
 import Navigation from '../components/Navigation';
 import styles from './Detail.module.css';
 
@@ -37,17 +36,19 @@ function Detail() {
               <img src={movie.large_cover_image} />
             </div>
 
-            <h1 className={styles.title}>{movie.title}</h1>
+            <div className={styles.infowrap}>
+              <h1 className={styles.title}>{movie.title}</h1>
 
-            <p className={styles.info}>
-              <span>({movie.year}) |</span>
-              <span>{movie.runtime}분 |</span>
-              <span>{movie.genres}</span>
-            </p>
+              <div>
+                <span>({movie.year}) |</span>
+                <span>{movie.runtime}분 |</span>
+                <span>{movie.genres}</span>
+              </div>
 
-            <p>평점 : ★ {movie.rating}</p>
+              <p className={styles.rating}>평점 : ★ {movie.rating}</p>
 
-            <p className={styles.description}>{movie.description_full}</p>
+              <p className={styles.description}>{movie.description_full}</p>
+            </div>
           </div>
         </div>
       )}
